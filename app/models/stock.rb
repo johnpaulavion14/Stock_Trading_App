e19374a68faa00cafb721d1b307ed7e8eb50d7ea
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 20:29:03 +0800

    Add validations to Stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+    validates :name, :symbol, :price, :percent_change, presence: true
+    validates :name, uniqueness: true, length: {maximum: 30}
+    validates :symbol, uniqueness: true, length: {maximum: 5}
+    validates :price, numericality: true
 end
