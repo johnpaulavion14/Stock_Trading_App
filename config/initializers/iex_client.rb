27c5ed902291299647187091ea461b24627f9441
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 19:36:08 +0800

    Add update stocks method

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
-    config.publishable_token = 'pk_ed649ff4ef704c2eb75311043162574e' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-    config.secret_token = 'sk_a568c6b678fc43149610cf56080aefe9' # defaults to ENV['IEX_API_SECRET_TOKEN']
+    config.publishable_token = 'pk_64fa39262f6f4570824e2d99b4b90248' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+    config.secret_token = 'sk_8d2a370755764be3b9404f3b70f3b40c' # defaults to ENV['IEX_API_SECRET_TOKEN']
     config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
