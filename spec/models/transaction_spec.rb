9201772c2214f853b2ce92c8d979018753a5fd72
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-14 11:34:25 +0800

    Edit rspec tests for model Transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
 
   describe "#symbol" do
+
+    before do
+      sign_in create(:user)
+    end
+
     it 'symbol should not be null' do
       transaction = create(:transaction)
       transaction.symbol = nil
@@ -26,6 +31,112 @@ RSpec.describe Transaction, type: :model do
       transaction.symbol = 'SYMBO'
       transaction.save
       expect(transaction).to be_valid
+    end
+  end
+
+  describe '#company' do
+    it 'company should not be null' do
+      transaction = create(:transaction)
+      transaction.company = nil
+      transaction.save
+      expect(transaction).to_not be_valid
+    end
+    it 'company should not be the same' do
+      transaction = create(:transaction)
+      transaction.company = "unique company"
+      transaction.save
+
+      transaction2 = create(:transaction)
+      transaction2.company = "unique company"
+      transaction2.save
+      expect(transaction2).to_not be_valid
+    end
+    it 'company should not be greater than 30 chars' do
+      transaction = create(:transaction)
+      transaction.company = "this is more than 30 characters"
+      transaction.save
+      expect(transaction).to_not be_valid
+    end
+  end
+
+  describe '#current_price' do
+    it 'current_price should not be null' do
+      transaction = create(:transaction)
+      transaction.current_price = nil
+      transaction.save
+      expect(transaction).to_not be_valid
+    end
+
+    it 'current_price should accept integer' do
+      transaction = create(:transaction)
+      transaction.current_price = 14
+      transaction.save
+      expect(transaction).to be_valid
+    end
+
+    it 'current_price should accept float' do
+      transaction2 = create(:transaction)
+      transaction2.current_price = 14.00
+      transaction2.save
+      expect(transaction2).to be_valid
+    end
+  end
+
+  describe '#commit' do
+    it 'commit should not be null' do
+      transaction = create(:transaction)
+      transaction.commit = nil
+      transaction.save
+      expect(transaction).to_not be_valid
+    end
+
+    it 'commit should be equal to buy' do
+      transaction = create(:transaction)
+      transaction.commit = 'buy'
+      transaction.save
+      expect(transaction).to be_valid
+    end
+
+    it 'commit should reject string not equal to buy' do
+      transaction2 = create(:transaction)
+      transaction2.commit = 'buyyy'
+      transaction2.save
+      expect(transaction2).to_not be_valid
+    end
+
+    it 'commit should be equal to sell' do
+      transaction = create(:transaction)
+      transaction.commit = 'sell'
+      transaction.save
+      expect(transaction).to be_valid 
+    end
+
+    it 'commit should reject string not equal to sell' do
+      transaction2 = create(:transaction)
+      transaction2.commit = 'selllll'
+      transaction2.save
+      expect(transaction2).to_not be_valid
+    end
+  end
+
+  describe '#shares' do
+    it 'shares should not be null' do
+      transaction = create(:transaction)
+      transaction.shares = nil
+      transaction.save
+      expect(transaction).to_not be_valid
+    end
+    it 'shares should accept integer only' do
+      transaction = create(:transaction)
+      transaction.shares = 13
+      transaction.save
+      expect(transaction).to be_valid
+    end
+    it 'shares should reject float' do
+      transaction2 = create(:transaction)
+      transaction2.shares = 13.00
+      transaction2.save
+      expect(transaction2).to_not be_valid
     end
   end
   
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
-  exit 1
+  exit 
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -37,7 +37,6 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
