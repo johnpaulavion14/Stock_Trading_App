7321ba7ad0ee708e94b5be6fc864d2efe1b442c6
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-16 15:02:23 +0800

    delete uniquiness: true in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,9 +2,9 @@ class Transaction < ApplicationRecord
     belongs_to :user
 
     validates :symbol, :shares, :company, :commit, :current_price, presence: true
-    validates :symbol, length: { maximum: 5 }, uniqueness: true
+    validates :symbol, length: { maximum: 5 }
     validates :commit, inclusion: { in: %w(buy sell), message: "%{value} is not valid" }
     validates :current_price, numericality: true
     validates :shares, numericality: { only_integer: true }
-    validates :company, length: { maximum: 30 }, uniqueness: true
+    validates :company, length: { maximum: 30 }
 end
