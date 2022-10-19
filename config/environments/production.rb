fc8664c17ab42a1c1cded15673cd34e9e69e3045
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-19 21:04:48 +0800

    Fix action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { 
+    :host => 'https://jp-stock.herokuapp.com/', 
+    :port => 8080
+  }
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'https://jp-stock.herokuapp.com',
+  user_name:            'actionmailer456@gmail.com',
+  password:             'qlklgdqhpkwaibes',
+  authentication:       'plain',
+  # enable_starttls_auto: true,
+ }
 end
