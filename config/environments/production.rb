eaaeaa91fa032a4b4d523b300e2748706cd11390
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-20 13:38:33 +0800

    Update action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "jp-stock.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "https://jp-stock.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { 
-    :host => 'jp-stock.herokuapp.com', 
-    :port => 'http'
-  }
-
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'jp-stock.herokuapp.com',
   user_name:            'actionmailer456@gmail.com',
   password:             'qlklgdqhpkwaibes',
   authentication:       'plain',
