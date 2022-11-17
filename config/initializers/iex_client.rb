0139a8c3160d7133e255c6c1aa6542dda13be293
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-11-17 22:24:58 +0800

    Change IEX token

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
-    config.publishable_token = 'pk_64fa39262f6f4570824e2d99b4b90248' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
-    config.secret_token = 'sk_8d2a370755764be3b9404f3b70f3b40c' # defaults to ENV['IEX_API_SECRET_TOKEN']
+    config.publishable_token = 'pk_6522fa45122342e88b8b3252a19ea8a7' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+    config.secret_token = 'sk_cf2b194d73374ffc924b1412bee05cd1' # defaults to ENV['IEX_API_SECRET_TOKEN']
     config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
 end
